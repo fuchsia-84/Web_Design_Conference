f846f286c846f08c2e0d2bd83999455e903c80c3
ふーしゃ / Fuchsia <46129202+fuchsia-84@users.noreply.github.com>
2023-07-12 01:17:03 +0900

    make toggle btn and global nav responsive.

---
+++ js/header_nav_toggle.js
@@ -0,0 +1,49 @@
+$(function() {
+    var $header = $('#header');
+    var change_header_position = $("#top").height() - $("#header").outerHeight(true);
+    // Nav Fixed
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > change_header_position) { 
+            $header.addClass('fixed');
+        } else {
+            $header.removeClass('fixed');
+        }
+    });
+
+    // Nav Toggle Button
+    $('#nav_toggle').click(function(){
+        const toggle_status = $('#nav_toggle').css('display');
+        if (toggle_status === "none") {
+            console.log('button is display none, do nothing');
+        } else {
+            $header.toggleClass('open');
+            // Global Nav
+            if ( $('#header').hasClass('open') ) {
+                $('#global_nav').slideDown();
+                $('#global_nav').css('display','block');
+            } else {
+                $('#global_nav').slideUp(function() {
+                    $('#global_nav').css('display','none');
+                });
+            }
+        }
+    });
+    // Global Nav Links
+    $('#global_nav li a').on('click', function(event){
+        $('#nav_toggle').trigger('click');
+    });
+
+    // For Resize
+    $(window).resize(function() {
+        var width = $(window).width();
+        if(width >= 769) {
+            $('#global_nav').css('display', 'block');
+            console.log('sp => tab or pc, global nav display block');
+        } else {
+            $('#global_nav').css('display', 'none');
+            console.log('pc or tab => sp, global nav display none');
+        }
+
+    });
+    
+});
