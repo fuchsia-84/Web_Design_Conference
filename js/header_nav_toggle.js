7866faeb9149a27b79d52a3a50d1e201d63a1337
ふーしゃ / Fuchsia <46129202+fuchsia-84@users.noreply.github.com>
2023-07-20 23:28:36 +0900

    add page top button.

--- js/header_nav_toggle.js
+++ js/header_nav_toggle.js
@@ -1,13 +1,23 @@
 $(function() {
     var $header = $('#header');
+    var $page_top_btn = $('#page_top_btn');
     var change_header_position = $("#top").height() - $("#header").outerHeight(true);
-    // Nav Fixed
+    var change_page_top_btn_position = 800;
+
+    // Nav and Page Top Btn Fixed
     $(window).scroll(function() {
         if ($(window).scrollTop() > change_header_position) { 
             $header.addClass('fixed');
         } else {
             $header.removeClass('fixed');
         }
+
+        if ($(window).scrollTop() > change_page_top_btn_position) {
+            $page_top_btn.addClass('fixed');
+        } else {
+            $page_top_btn.removeClass('fixed');
+        }
+
     });
 
     // Nav Toggle Button
